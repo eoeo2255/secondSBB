bf806f3590ac4651ccee895c316d3f56f9a62d98
eoeo2255 <naiga6@gmail.com>
2023-03-27 15:27:08 +0900

    CK:í데이터 추가

--- sbb2/src/main/java/com/mysite/sbb2/Question.java
+++ sbb2/src/main/java/com/mysite/sbb2/Question.java
@@ -14,10 +14,13 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(length = 200)
     private String subject;
+
     @Column(columnDefinition = "TEXT")
     private String content;
+
     private LocalDateTime createDate;
     @OneToMany(mappedBy = "question", cascade =CascadeType.REMOVE)
     private List<Answer> answerList;
--- sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
+++ sbb2/src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
@@ -1,13 +1,30 @@
 package com.mysite.sbb2;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class Sbb2ApplicationTests {
-
+	@Autowired
+	private QuestionRepository questionRepository;
 	@Test
-	void contextLoads() {
+	@DisplayName("질문 등록")
+	void test() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
 	}
 
 }
