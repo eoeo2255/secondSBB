33dd188be81908c7cb9ef639a8e311a0bc4b72c2
eoeo2255 <naiga6@gmail.com>
2023-03-29 10:45:36 +0900

    í3-06 회원가입

---
+++ sbb2/src/main/java/com/mysite/sbb2/user/SiteUser.java
@@ -0,0 +1,20 @@
+package com.mysite.sbb2.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    @Column(unique = true)
+    private String email;
+}
